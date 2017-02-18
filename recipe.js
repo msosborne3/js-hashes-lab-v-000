8683d8505859555c514661d11a60ef38d54c487a
msosborne3 <msosborne@ymail.com>
2017-02-17 22:38:44 -0600

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,25 @@
 'use strict';
+
+// Recipe is an object, ingredient is a string representing the
+// name of the ingredient, and amount is a number representing the
+// amount of the ingredient.
+function addIngredient(recipe, ingredient, amount) {
+  recipe[ingredient] = amount;
+  return recipe;
+}
+
+function removeIngredient(recipe, ingredient) {
+  delete recipe[ingredient];
+  return recipe;
+}
+
+function updateIngredient(recipe, ingredient, amount) {
+  recipe[ingredient] = amount;
+  return recipe;
+}
+
+function readRecipe(recipe) {
+  for(var key in recipe) {
+    console.log(`this recipe calls for ${recipe[key]} of ${key}`);
+  };
+}
